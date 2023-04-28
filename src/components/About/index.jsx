a2c078b9afba8eb1070323a3f6cc9eeef831912b
mhmtkarakas <m.karakas32@gmail.com>
2023-04-28 22:31:56 +0300

    page loader built

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -10,11 +10,13 @@ import {
     faJsSquare,
     faReact,
   } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders';
 
 
 const About =()=>{
     const [letterClass,setLetterClass] = useState('text-animate')
     return (
+        <>
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
@@ -58,6 +60,8 @@ const About =()=>{
             </div>
 
         </div>
+        <Loader type='pacman' /> 
+        </>
     )
 }
 
